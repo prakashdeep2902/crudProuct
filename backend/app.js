d8c0ee4967c7d18e6161926c4df92d7d7c1856d2
sharmaJi <prakashdeep2902@gmail.com>
2026-05-14 15:04:59 +0530

    added new module

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
 dotenv.config();
 import router from "./src/index.js";
 const app = express();
 import db from "./db.js";
+app.use(cors());
+// app.use(helmet());
 app.use(express.json());
 
 app.use("/api", router);
